eba19bcf9724cf0db3f12eb939f71f6fbb25f16d
Yakup Zengin <clafedersin@gmail.com>
2023-12-26 17:29:09 +0300

    Updated comment line : Test.java

--- Test.java
+++ Test.java
@@ -3,15 +3,15 @@ import java.util.Map;
 
 public class Test {
     public static void main(String[] args) throws Exception {
+            // Step 1: Reads characters and calculates their frequencies from the letter file.
+            Map<String, Integer> frequencies = FrequencyReader.calculateFruquencies("letter.txt");
 
-            // Step 1: Read characters and frequencies from letter.txt
-            Map<String, Integer> frequencies = FrequencyReader.readFrequencies("letter.txt");
-
-            // Step 2: Create a linked list and insert frequencies in sorted order
+            // Step 2: Creates a linked list and inserts frequiencies in sorted order.
             LinkedListHuffman linkedList = new LinkedListHuffman();
             for (String key : frequencies.keySet()) {
                 linkedList.insertSorted(new HuffmanNode(key, frequencies.get(key)));
             }
+            // Displays the characters and their frequencies in the linked list
             System.out.println("Character and their frequiencies");
             linkedList.display();
 
@@ -19,20 +19,19 @@ public class Test {
             HuffmanTree huffmanTree = new HuffmanTree();
             HuffmanNode root = huffmanTree.createHuffmanTree(linkedList);
 
-            // Step 4: Display Huffman codes
-            System.out.println("The code of every character");
+            // Step 4: Display Huffman codes for each characher
+            System.out.println("Huffman codes for each characher");
             Map<String, String> huffmanCodes = huffmanTree.getHuffmanCodes(root, "");
             for (String key : huffmanCodes.keySet()) {
                 String value = huffmanCodes.get(key);
                 System.out.println(key + ": " + value);
             }
-            System.out.println();
 
 
-            // Step 5: Encode source text and write to encoded.txt
+            // Step 5: Encode the source text and write to encoded.txt
             HuffmanEncoder.encodeAndWrite("source.txt", "encoded.txt", huffmanCodes);
 
-            // Step 6: Decode encoded text and write to decoded.txt
+            // Step 6: Decode the encoded text and write to decoded.txt
             HuffmanDecoder.decodeAndWrite("encoded.txt", "decoded.txt", root);
 
         }
