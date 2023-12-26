f7f9ddc72a0c6c6d6e221f7ab6d665e70e4f50b7
Yakup Zengin <clafedersin@gmail.com>
2023-12-26 17:28:49 +0300

    added print part : HuffmanEncoder.java

--- HuffmanEncoder.java
+++ HuffmanEncoder.java
@@ -8,6 +8,7 @@ public class HuffmanEncoder {
     public static void encodeAndWrite(String sourceFileName, String encodedFileName, Map<String, String> huffmanCodes) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(sourceFileName));
         BufferedWriter writer = new BufferedWriter(new FileWriter(encodedFileName));
+        BufferedReader readerCodes = new BufferedReader(new FileReader(encodedFileName));
 
         int ch;
 
@@ -21,9 +22,20 @@ public class HuffmanEncoder {
             // Writes the Huffman code to the encoded file.
             writer.write(code);
         }
+        writer.close();
+
+
+        System.out.print("Encoded : ");
+        int ch2;
+        while ((ch2 = readerCodes.read()) != -1) {
+
+            // Converts the character to a string.
+            String code = String.valueOf((char) ch2);
+            System.out.print(code);
+        }
+        System.out.println();
         System.out.println("Encoded the given txt and writed the coded text into encoded.txt file.");
 
         reader.close();
-        writer.close();
     }
 }
