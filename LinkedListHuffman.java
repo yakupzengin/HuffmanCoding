26dfa60ac8391329c9278a589044519ff79f48b9
Yakup Zengin <clafedersin@gmail.com>
2023-12-26 18:23:26 +0300

    Update variable :  LinkedListHuffman.java

--- LinkedListHuffman.java
+++ LinkedListHuffman.java
@@ -2,28 +2,28 @@ class LinkedListHuffman {
     Node<HuffmanNode> head;
 
     // Inserts a HuffmanNode into the sorted linked list based on frequency.
-    public void insertSorted(HuffmanNode node) {
-
+    public void insertSorted(HuffmanNode newHNode) {
         // Create a new LinkedList Node with the given HuffmanNode.
-        Node<HuffmanNode> newNode = new Node<>(node);
+        Node<HuffmanNode> newNode = new Node<>(newHNode);
 
         // If the linkedlist is empty or the node's frequency is smaller than the head's frequency,
         // the new linkedlist Node becomes the head.
-        if (head == null || node.frequency < head.node.frequency) {
+        if (head == null || newHNode.frequency < head.node.frequency) {
             newNode.next = head;
             head = newNode;
         } else {
-            Node<HuffmanNode> current = head;
+            Node<HuffmanNode> iterator = head;
             Node<HuffmanNode> prev = null;
 
-            while (current != null && node.frequency >= current.node.frequency) {
-                prev = current;
-                current = current.next;
+            // Pay attention to the order within the loop!
+            while (iterator != null && newHNode.frequency >= iterator.node.frequency) {
+                prev = iterator;
+                iterator = iterator.next;
             }
 
             // Insert the new node in the sorted position.
             prev.next = newNode;
-            newNode.next = current;
+            newNode.next = iterator;
         }
     }
 
